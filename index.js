972985dc33085bd977d25eebd93db4685f5fb225
Grace <gracenyangayi@gmail.com>
2022-06-09 21:18:33 +0300

    commit tree 473d41874fdf9bbdb37b817a33bca1672fb8fd6b

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Code your solution here
+const drivers = [
+  {
+    name: 'Bobby',
+    hometown: 'Pittsburgh'
+  },
+  {
+    name: 'Sammy',
+    hometown: 'New York'
+  },
+  {
+    name: 'Sally',
+    hometown: 'Cleveland'
+  },
+  {
+    name: 'Annette',
+    hometown: 'Los Angeles'
+  },
+  {
+    name: 'Bobby',
+    hometown: 'Tampa Bay'
+  }
+];
+const findMatching = (driver, check) => {
+  return driver.filter((match) => match.toLowerCase() === check.toLowerCase()
+
+  )
+}
+
+
+const fuzzyMatch = (driver, checkString) => {
+  return driver.filter((match) => match.toLowerCase().indexOf(checkString.toLowerCase()) === 0)
+
+
+}
+const matchName = (driver, checkName) => {
+  return driver.filter((record) => record.name === checkName)
+}
